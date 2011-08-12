9339300bd92c11b16fa0140b31fe95e49d7f1442
root <root@server2.mywebdesign.ie>
2011-08-12 15:03:56 +0100

    First Commit

---
+++ socket.php
@@ -0,0 +1,71 @@
+<?php
+/* 
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+/**
+* Description of socket
+*
+* @author Philip Brookes
+*/
+class socket {
+	private $ip;
+	private $port;
+	private $sockHandle;
+
+	public function getSock(){
+		return $this->sockHandle;
+	}
+	public function setIP($newVal){
+		$this->ip = $newVal;
+	}
+	public function getIP(){
+		return $this->ip;
+	}
+
+	public function setPort($newVal){
+		$this->port = $newVal;
+	}
+	public function getPort(){return $this->port;}
+
+	public function __construct($ip=null, $port=null){
+		$this->sockHandle = socket_create(AF_INET, SOCK_STREAM, 0);
+		socket_set_nonblock($this->sockHandle);
+		$this->setPort($port);
+		$this->setIP($ip);
+	}                                                                                            
+
+	public function open($ip=null, $port=null){                                                                                                                                                  
+		if($ip)         $this->setIP($ip);                                                                                                                                                   
+		if($port)       $this->setPort($port);                                                                                                                                               
+		if(socket_bind($this->sockHandle, $this->getIP(), $this->getPort())){                                                                                                                
+			socket_listen($this->sockHandle);
+			return true;
+		}
+		return false;
+	}
+
+	public function accept($socket=null){
+		if(is_null($socket)) return false;
+		$newconn = @socket_accept($socket);
+		if($newconn == false) return false;
+		else $this->sockHandle = $newconn;
+		socket_set_nonblock($this->sockHandle);
+		return true;
+	}
+
+	public function getData(){
+		$data = @socket_read($this->sockHandle, 1024);
+		return $data;
+	}
+
+	public function write($data){
+		socket_write($this->sockHandle, $data);
+	}
+
+	public function close(){
+		socket_close($this->sockHandle);
+	}
+
+}
