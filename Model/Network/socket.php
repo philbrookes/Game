e9264497bb353c5e135bee7baedc572df60fb4db
phil brookes <phil@mywebdesign.ie>
2011-08-13 14:06:45 +0100

    d'oh!
    Signed-off-by: phil brookes <phil@mywebdesign.ie>

--- Model/Network/socket.php
+++ Model/Network/socket.php
@@ -57,7 +57,7 @@ class socket {
 	}
 	
 	public function isAlive(){
-	    $this->write("");
+	    $this->write(" ");
 	    $result = socket_last_error($this->sockHandle);
 	    echo "got $result...\n";
 	    if($result !== false){
