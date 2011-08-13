8f48a409ac146d2802b6e6ae6164d4d9a07b63a5
U-PHIL\philip <philip@phil.(none)>
2011-08-13 19:22:49 +0100

    formatting welcome message

--- Controller/Command/welcome.php
+++ Controller/Command/welcome.php
@@ -5,6 +5,9 @@ use Model\Utility\registry;
 
 class welcome extends abCommand{
     public function processCommand(instruction $instruction) {
-	$instruction->getPlayer()->sendData("Welcome to the Game...\nTo talk to other users use: blurt <message>\nTo set your own name use: name <name> (only the first word will be used!)n\Have fun...");
+	$instruction->getPlayer()->sendData("Welcome to the Game...
+To talk to other users use: blurt <message>
+To set your own name use: name <name> (only the first word will be used!)
+Have fun...");
     }
 }
