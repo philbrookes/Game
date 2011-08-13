2789c0ad5b488e5e974c1e8e677b02515059ffbd
U-PHIL\philip <philip@phil.(none)>
2011-08-13 19:21:25 +0100

    newlines in welcome message

--- Controller/Command/welcome.php
+++ Controller/Command/welcome.php
@@ -5,9 +5,6 @@ use Model\Utility\registry;
 
 class welcome extends abCommand{
     public function processCommand(instruction $instruction) {
-	$instruction->getPlayer()->sendData("Welcome to the Game...");
-        $instruction->getPlayer()->sendData("To talk to other users use: blurt <message>");
-        $instruction->getPlayer()->sendData("To set your own name use: name <name> (only the first word will be used!)");
-        $instruction->getPlayer()->sendData("Have fun...");
+	$instruction->getPlayer()->sendData("Welcome to the Game...\nTo talk to other users use: blurt <message>\nTo set your own name use: name <name> (only the first word will be used!)n\Have fun...");
     }
 }
