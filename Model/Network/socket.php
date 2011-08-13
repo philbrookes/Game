c1ed2535de09ac9615afbb1eab2696034767e971
phil brookes <phil@mywebdesign.ie>
2011-08-13 17:14:21 +0100

    testing blank chars
    Signed-off-by: phil brookes <phil@mywebdesign.ie>

--- Model/Network/socket.php
+++ Model/Network/socket.php
@@ -57,7 +57,7 @@ class socket {
 	}
 	
 	public function isAlive(){
-	    $this->write("");
+	    $this->write(chr(8));
 	    $result = socket_last_error($this->sockHandle);
 	    echo "got $result...\n";
 	    if($result == 32 || $result == 104){
