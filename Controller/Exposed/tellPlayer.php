db270bb6cdd69baa751a16354da245cf7de987c4
U-PHIL\philip <philip@phil.(none)>
2011-08-14 04:28:38 +0100

    debugging parser

--- Controller/Exposed/tellPlayer.php
+++ Controller/Exposed/tellPlayer.php
@@ -10,13 +10,13 @@ use Model\Utility\registry;
 
 class tellPlayer {
     public function process($args){
-        echo "running tellPlayer with...\n";
-        print_r($args);
         $playerId = $arg[0];
         $msg = $arg[1];
         $players = registry::getObject("players");
         foreach($players as $player){
+            echo $player->getId()." == ".$playerId."\n";
             if($player->getId() == $playerId){
+                echo "sending $msg to ".$player->getName()."\n";
                 $player->sendData($msg);
                 break;
             }
